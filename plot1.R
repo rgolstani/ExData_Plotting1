a42c6699178633c97b99654acb777e7e83992f31
Roshana Golstani <rgolstani@yahoo.com>
2015-02-15 07:27:12 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,17 @@
+##### The required data is downloaded in workind directory in zip format
+
+setwd("~/Desktop/ExploratoryCoursera")
+# Read data set after unzipping the data file
+pwruse <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"),
+                               header=T, sep=";")
+# Household power consumption for Feb. 1 and 2, 2007 only
+pwrusetwodays <- pwruse[as.character(pwruse$Date) %in% c("1/2/2007", "2/2/2007"),]
+# Concatante Date and Time variables
+pwrusetwodays$dateTime = paste(pwrusetwodays$Date, pwrusetwodays$Time)
+# Convert to Date/Time class
+pwrusetwodays$dateTime <- strptime(pwrusetwodays$dateTime, "%d/%m/%Y %H:%M:%S")
+attach(pwrusetwodays)
+png("plot1.png", width=480, height=480, units="px")
+# Plot the distribution of global active power
+hist(as.numeric(as.character(Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
