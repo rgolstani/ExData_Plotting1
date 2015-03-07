5bdbf6a12c64dff79f7e786d24a99c042d53c9ef
Roshana Golstani <rgolstani@yahoo.com>
2015-03-06 20:54:13 -0600

    Update project1plot2.R

--- project1plot2.R
+++ project1plot2.R
@@ -1,13 +1,13 @@
 
 
 
-setwd("~/Desktop/ExploratoryCoursera")
+setwd("~/Desktop/ExploratoryCoursera") ## working directory
 # Read data set after unzipping the data file
 pwruse <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"),
                      header=T, sep=";")
 # Household power consumption for Feb. 1 and 2, 2007 only
 pwrusetwodays <- pwruse[as.character(pwruse$Date) %in% c("1/2/2007", "2/2/2007"),]
-# Concatante Date and Time variables
+# paste Date and Time variables
 pwrusetwodays$dateTime = paste(pwrusetwodays$Date, pwrusetwodays$Time)
 # Convert to Date/Time class
 pwrusetwodays$dateTime <- strptime(pwrusetwodays$dateTime, "%d/%m/%Y %H:%M:%S")
