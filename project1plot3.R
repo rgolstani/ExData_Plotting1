a0aa4ca8597b92fa8f85d25a6443b21a66ecfa2c
Roshana Golstani <rgolstani@yahoo.com>
2015-03-07 15:22:19 -0600

    Update project1plot3.R

--- project1plot3.R
+++ project1plot3.R
@@ -1,17 +1,12 @@
 
-# Change working directory
-#setwd("EDA/Projects/Project1/dataCodes")
-# Url at which the data set is located
-#fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-# Download zip file
-#download.file(fileUrl, destfile="household_power_consumption.zip", method="curl")
+# moving to the directory where the downloaded data file exdata_data_household_power_consumption.zip is
 setwd("~/Desktop/ExploratoryCoursera")
 # Read data set after unzipping the data file
 pwruse <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"),
                      header=T, sep=";")
 # Household power consumption for Feb. 1 and 2, 2007 only
 pwrusetwodays <- pwruse[as.character(pwruse$Date) %in% c("1/2/2007", "2/2/2007"),]
-# Concatante Date and Time variables
+# paste Date and Time variables
 pwrusetwodays$dateTime = paste(pwrusetwodays$Date, pwrusetwodays$Time)
 # Convert to Date/Time class
 pwrusetwodays$dateTime <- strptime(pwrusetwodays$dateTime, "%d/%m/%Y %H:%M:%S")
